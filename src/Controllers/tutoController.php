8810f3f0990c06f84ee09f01faecb771bd63b550
Alexis CALS <alexiscals20@gmail.com>
2022-05-06 13:53:18 +0200

    first commit

---
+++ src/Controllers/tutoController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace tutoAPI\Controllers;
+
+use tutoAPI\Models\TutoManager;
+use tutoAPI\Models\Tuto;
+use tutoAPI\Controllers\abstractController;
+
+class tutoController extends abstractController
+{
+
+    public function show($id)
+    {
+
+        // Données issues du Modèle
+
+        $manager = new TutoManager();
+
+        $tuto = $manager->find($id);
+
+        // Template issu de la Vue
+
+        return $this->jsonResponse($tuto, 200);
+    }
+
+    public function index($page) {
+        $manager = new TutoManager();
+
+        switch($page) {
+          case 1:
+            $a = 1;
+            $b = 4;
+            break;
+          case 2:
+            $a = 5;
+            $b = 8;
+            break;
+            case 3:
+              $a = 9;
+              $b = 12;
+              break;
+        }
+
+        $tutos = $manager->findAll($a, $b);
+
+        return $this->jsonResponse($tutos, 200);
+    }
+
+    public function add()
+    {
+        // Ajout d'un tuto
+      $tuto = new Tuto();
+      $manager = new TutoManager();
+
+      $now = new \DateTime();
+      $dateString = date( 'Y-m-d', $now->getTimestamp());
+
+      $tuto->setTitle($_POST['title']);
+      $tuto->setDescription($_POST['description']);
+      $tuto->setCreatedAt($_POST['createdAt']);
+      $tuto->setCreatedAt($dateString);
+
+      $tuto = $manager->add($tuto);
+
+      return $this->jsonResponse($tuto, 201);
+    }
+
+    public function update($id) {
+
+      $manager = new TutoManager();
+      $tuto = $manager->find($id);
+
+      parse_str(file_get_contents('php://input'), $_PATCH);
+      var_dump($_PATCH);
+
+      foreach ($_PATCH as $key => $value){
+        if (!empty($key) and $key == "title"){
+          $tuto->setTitle($_PATCH['title']);
+        }
+        elseif (!empty($key) and $key == "description"){
+          $tuto->setDescription($_PATCH['description']);
+        }
+
+      }
+
+      $newTuto = $manager->update($tuto);
+      return $this->jsonResponse($newTuto, 200);
+    }
+
+    public function delete($id) {
+        // Données issues du Modèle
+        $manager = new TutoManager();
+
+        $tuto = $manager->find($id);
+        $manager->delete($id);
+
+        return $this->jsonResponse($tuto, 200);
+    }
+
+
+}
---
+++ src/Models/TutoManager.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace tutoAPI\Models;
+
+use tutoAPI\Services\Manager;
+
+class TutoManager extends Manager
+{
+
+    public function find($id)
+    {
+
+        // Connexion à la BDD
+        $dbh = static::connectDb();
+
+        // Requête
+        $sth = $dbh->prepare('SELECT * FROM tutos WHERE id = :id');
+        $sth->bindParam(':id', $id, \PDO::PARAM_INT);
+        $sth->execute();
+        $result = $sth->fetch(\PDO::FETCH_ASSOC);
+
+        // Instanciation d'un tuto
+        $tuto = new Tuto();
+        $tuto->setId($result["id"]);
+        $tuto->setTitle($result["title"]);
+        $tuto->setDescription($result["description"]);
+        $tuto->setCreatedAt($result["createdAt"]);
+
+        // Retour
+        return $tuto;
+    }
+
+    public function findAll()
+    {
+
+        // Connexion à la BDD
+        $dbh = static::connectDb();
+
+        // Requête
+        $sth = $dbh->prepare('SELECT * FROM tutos');
+        $sth->execute();
+
+        $tutos = [];
+
+        while($row = $sth->fetch(\PDO::FETCH_ASSOC)){
+
+            $tuto = new Tuto();
+            $tuto->setId($row['id']);
+            $tuto->setTitle($row['title']);
+            $tuto->setDescription($row['description']);
+            $tuto->setCreatedAt($row["createdAt"]);
+            $tutos[] = $tuto;
+
+        }
+
+        return $tutos;
+
+    }
+
+    public function add(Tuto $tuto) {
+
+        // Connexion à la BDD
+        $dbh = static::connectDb();
+
+        $title = $tuto->getTitle();
+        $description = $tuto->getDescription();
+        $createdAt = $tuto->getCreatedAt();
+
+        // Requête
+        $sth = $dbh->prepare('INSERT INTO tutos (title, description, createdAt) VALUES (:title, :description, :createdAt)');
+        $sth->bindValue(':title', $title);
+        $sth->bindValue(':description', $description);
+        $sth->bindValue(':createdAt', $createdAt);
+        $sth->execute();
+
+        // Retour
+        $id = $dbh->lastInsertId();
+        $tuto->setId($id);
+        return $tuto;
+
+    }
+
+    public function update(Tuto $tuto) {
+      // Connexion à la BDD
+      $dbh = static::connectDb();
+
+      $id = $tuto->getId();
+      $title = $tuto->getTitle();
+      $description = $tuto->getDescription();
+      $createdAt = $tuto->getCreatedAt();
+
+      // Requête
+      $sth = $dbh->prepare('UPDATE tutos SET title = :title, description = :description, createdAt = :createdAt WHERE id = :id');
+      $sth->bindValue(':id', $id);
+      $sth->bindValue(':title', $title);
+      $sth->bindValue(':description', $description);
+      $sth->bindValue(':createdAt', $createdAt);
+      $sth->execute();
+
+      // Retour
+      return $tuto;
+
+    }
+
+    public function delete($id) {
+        // Connexion à la BDD
+        $dbh = static::connectDb();
+
+        // Requête
+        $sth = $dbh->prepare('DELETE FROM tutos WHERE id = :id');
+        $sth->bindValue(':id', $id);
+        $sth->execute();
+    }
+}
